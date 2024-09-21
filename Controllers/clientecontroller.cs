982b13ab6ba0e0525d6144c27770a7f9114016e4
Sergi <Sergi@DESKTOP-9QFVC0K>
2024-09-21 10:34:58 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/clientecontroller.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.models;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("cliente")]
+    public class clientecontroller : ControllerBase
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarcliente()
+        {
+            List<cliente> clientes = new List<cliente>
+            {
+                new cliente
+
+                {
+                    id="1",
+                    correo="pruebas1@gmail.com",
+                    edad="24",
+                    nombre="juan"
+                }
+
+                /* new cliente
+
+                {
+                    id="2",
+                    correo="pruebas2@gmail.com",
+                    edad="24",
+                    nombre="pepe"
+                }
+                */
+
+            };
+
+            return clientes;
+
+
+
+            //}
+            //[HttpPost]
+            //[Route("guardar")]
+            //public dynamic guardarcliente()
+
+
+
+            //}
+        }
+    }
+}
---
+++ models/semilla.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2.models
+{
+    public class semilla
+    {
+        public string nombre { get; set; }
+        public string id { get; set; }
+        public string cantidad { get; set; }
+    }
+}
